35a7bcab583f0b8396557b17be7d2474ab5be8c7
Gurinder Bhangu <gbhangu@gbhangu.hitronhub.home>
2019-12-29 11:00:06 -0600

    app build and runs as expected after chaning to androidx from android.support

--- app/src/main/java/com/example/android/whatstheweather/DisplayMessageActivity.java
+++ app/src/main/java/com/example/android/whatstheweather/DisplayMessageActivity.java
@@ -4,9 +4,10 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 import android.widget.VideoView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/android/whatstheweather/MainActivity.java
+++ app/src/main/java/com/example/android/whatstheweather/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.whatstheweather;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -10,14 +11,15 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import org.json.JSONArray;
 import org.json.JSONException;
